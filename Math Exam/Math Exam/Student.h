6b8df3b8216f845ebc2d54927462b54cd397ffa4
DimDimch <60793570+DimDimch@users.noreply.github.com>
2021-05-18 14:40:33 +0300

    Version 2

--- Math Exam/Math Exam/Student.h
+++ Math Exam/Math Exam/Student.h
@@ -5,17 +5,19 @@
 class GoodStudent : public Human {
 public:
 	GoodStudent(string _name) : Human(_name) {}
-	void SolveEquations(vector<QuadricEquation>& tasks, list<Solution>& ans);
+	void SolveEquations(const vector<QuadricEquation>& tasks, list<Solution>& ans) override;
 };
 
 class NormalStudent : public Human {
 public:
 	NormalStudent(string _name) : Human(_name) {}
-	void SolveEquations(vector<QuadricEquation>& tasks, list<Solution>& ans);
+	void SolveEquations(const vector<QuadricEquation>& tasks, list<Solution>& ans) override;
+private:
+	const int m_errorChance = 63;
 };
 
 class BadStudent : public Human {
 public:
 	BadStudent(string _name) : Human(_name) {}
-	void SolveEquations(vector<QuadricEquation>& tasks, list<Solution>& ans);
+	void SolveEquations(const vector<QuadricEquation>& tasks, list<Solution>& ans) override;
 };
